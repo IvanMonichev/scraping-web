a1658cd76b56f9144be0d18f88819bded15a7aad
IvanMonichev <monichevid@baltech.ru>
2024-10-31 21:40:47 +0300

    feat: add parsing for titles

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import ssl
 from urllib import request, parse
 
+from bs4 import BeautifulSoup
+
 # Constants
 BASE_URL = "https://habr.com"
 SEARCH_PATH = "/ru/search/"
@@ -11,17 +13,18 @@ TARGET_TYPE_VALUE = "posts"
 ORDER_PARAM = "order"
 ORDER_VALUE = "relevance"
 
-# Кодирование поискового запроса
 encoded_search_term = parse.quote(SEARCH_TERM)
 
-# Собираем URL
 URL_STRING = f"{BASE_URL}{SEARCH_PATH}?{QUERY_PARAM}={encoded_search_term}&{TARGET_TYPE_PARAM}={TARGET_TYPE_VALUE}&{ORDER_PARAM}={ORDER_VALUE}"
 
-
-# Отключение проверки сертификатов для учебных целей
+# Disable SSL for learning task
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
 html = request.urlopen(URL_STRING, context=ctx)
-print(html.read().decode('utf-8'))
+bs = BeautifulSoup(html.read(), 'html.parser')
+
+titles = bs.find_all("h2", class_="tm-title tm-title_h2")
+for title in titles:
+    print(title.get_text(strip=True))
